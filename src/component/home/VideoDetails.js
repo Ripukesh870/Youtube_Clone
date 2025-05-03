426a57b41d05e233d4b43d2fdb881f676f1aef55
Ripukesh Kumar <ripukesh870@gmail.com>
2025-05-03 11:09:08 +0530

    few change

--- src/component/home/VideoDetails.js
+++ src/component/home/VideoDetails.js
@@ -16,9 +16,7 @@ function VideoDetails() {
     const currentVideo = useSelector((state) => state.videoReducer.currentVideo);
     const history = useSelector((state) => state.history.history);
 
-    // console.log(currentVideo.title);
     const handlePlay=(items)=>{
-        // console.log(items);
         dispach(videoPlay(items));
         nav(`/video/${items.id}`)
         dispach(addToHistory(items));
@@ -27,8 +25,8 @@ function VideoDetails() {
 
     console.log(history);
     return (
-        <div className="w-full lg:flex flex-row lg:w-4/5 md:w-3/4 sm:w-2/3 xl:w-5/6 overflow-y-scroll ">
-            <div className="  xl:w-3/4 lg:w-2/3 mt-2 p-4 border-2 ">
+        <div className="w-full lg:flex flex-row lg:w-4/5 md:w-3/4 sm:w-2/3 xl:w-5/6 overflow-y-scroll scrollbar-none ">
+            <div className="  xl:w-3/4 lg:w-2/3 mt-2 p-4 ">
                 <div className="w-full p-2  ">
                     <video
                         src={currentVideo.videoUrl}
@@ -59,10 +57,10 @@ function VideoDetails() {
                     </div>
                 </div>
             </div>   
-            <div className="px-2 w-full lg:w-1/3 h-screen  lg:overflow-y-scroll lg:scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-gray-500 py-4 xl:w-1/4" style={{borderLeft:"2px solid #3d3d3b"}}>
+            <div className="px-2 w-full lg:w-1/3 h-screen lg:overflow-y-scroll lg:scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-gray-500 py-4 xl:w-1/4" style={{borderLeft:"2px solid #3d3d3b"}}>
                 {  
                     data.map((items) => (
-                        <div className="w-full flex p-2 hover:bg-zinc-800 hover:rounded-md px-4 gap-2 cursor-pointer rounded-lg  my-3 bg-zinc-900 hover:bg-zinc-850 border-2" onClick={()=>handlePlay(items)} style={{}} key={items.id}>
+                        <div className="w-full flex p-2 hover:bg-zinc-800 hover:rounded-md px-4 gap-2 cursor-pointer rounded-lg  my-3 bg-zinc-900 hover:bg-zinc-850 " onClick={()=>handlePlay(items)} style={{}} key={items.id}>
                             <div className="w-2/5 rounded-md h-24" style={{border:"2px solid #3d3d3b"}}>
                                 <img src={items.thumbnailUrl} alt="" className="rounded-md w-full h-full" />
                             </div>
